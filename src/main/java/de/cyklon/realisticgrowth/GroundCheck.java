6f435ba0a939d639c779b28f8daefb77a53c8ba5
cyklon73 <cyklon698@gmail.com>
2024-12-25 12:09:30 +0100

    implemented check for large trees (2x2)

--- src/main/java/de/cyklon/realisticgrowth/GroundCheck.java
+++ src/main/java/de/cyklon/realisticgrowth/GroundCheck.java
@@ -1,5 +1,7 @@
 package de.cyklon.realisticgrowth;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -20,6 +22,65 @@ public class GroundCheck {
 
 	public static GroundCheck checkTreeGround() {
 		return checkBelow(Material.DIRT, Material.GRASS_BLOCK, Material.COARSE_DIRT, Material.PODZOL, Material.MOSS_BLOCK, Material.MYCELIUM, Material.ROOTED_DIRT);
+	}
+
+	public static GroundCheck checkLargeTreeGround() {
+		/*
+		x ->    z |
+
+		1		1 2
+				3 X
+
+		2		1 2
+				X 4
+
+		3		X 2
+				3 4
+
+		4		1 X
+				3 4
+		 */
+		return new GroundCheck((gc, l) -> {
+			LargeField f_1 = new LargeField(
+					checkTreeGround(),
+					l.clone().add(-1, 1, -1),
+					l.clone().add(0, 1, -1),
+					l.clone().add(-1, 1, 0),
+					l.clone().add(0, 1, 0)
+			);
+
+			LargeField f_2 = new LargeField(
+					checkTreeGround(),
+					l.clone().add(0, 1, -1),
+					l.clone().add(1, 1, -1),
+					l.clone().add(0, 1, 0),
+					l.clone().add(1, 1, 0)
+			);
+
+			LargeField f_3 = new LargeField(
+					checkTreeGround(),
+					l.clone().add(0, 1, 0),
+					l.clone().add(1, 1, 0),
+					l.clone().add(0, 1, 1),
+					l.clone().add(1, 1, 1)
+			);
+
+			LargeField f_4 = new LargeField(
+					checkTreeGround(),
+					l.clone().add(-1, 1, 0),
+					l.clone().add(0, 1, 0),
+					l.clone().add(-1, 1, 1),
+					l.clone().add(0, 1, 1)
+			);
+
+			for (LargeField f : Arrays.asList(f_1, f_2, f_3, f_4)) {
+				if (f.check()) {
+					gc.field = f;
+					return true;
+				}
+			}
+			return false;
+		});
 	}
 
 	public static GroundCheck checkMangroveGround() {
@@ -86,5 +147,33 @@ public class GroundCheck {
 	private GroundCheck and(BiPredicate<GroundCheck, Location> check) {
 		this.check = this.check.and(check);
 		return this;
+	}
+
+	public LargeField getField() {
+		LargeField field = this.field;
+		this.field = null;
+		return field;
+	}
+
+	@Getter
+	@AllArgsConstructor
+	public static class LargeField {
+
+		private final GroundCheck check;
+		private final Location location_1;
+		private final Location location_2;
+		private final Location location_3;
+		private final Location location_4;
+
+		public List<Location> getLocations() {
+			return Arrays.asList(location_1, location_2, location_3, location_4);
+		}
+
+		public boolean check() {
+			return check.check(location_1) &&
+					check.check(location_2) &&
+					check.check(location_3) &&
+					check.check(location_4);
+		}
 	}
 }
