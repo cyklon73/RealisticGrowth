71799f8a25df20a27e6e7451767ff728313b37a6
cyklon73 <cyklon698@gmail.com>
2024-12-25 12:13:22 +0100

    implemented correct placing of large flowers (1x2)

--- src/main/java/de/cyklon/realisticgrowth/RealisticGrowth.java
+++ src/main/java/de/cyklon/realisticgrowth/RealisticGrowth.java
@@ -1,22 +1,27 @@
 package de.cyklon.realisticgrowth;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ItemDespawnEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.function.Consumer;
 
 public final class RealisticGrowth extends JavaPlugin implements Listener {
 
-    private Map<Material, GroundCheck> saplings;
+    private static final String UPPER = "[half=upper]";
+
     private Map<Material, SaplingData> saplings;
     private Map<Material, Material> replaces;
+    private Map<Material, Consumer<Location>> placeHandler;
 
     private FileConfiguration config;
 
@@ -30,6 +35,7 @@ public final class RealisticGrowth extends JavaPlugin implements Listener {
 
         this.saplings = new HashMap<>();
         this.replaces = new HashMap<>();
+        this.placeHandler = new HashMap<>();
 
         this.config = getConfig();
 
@@ -55,9 +61,11 @@ public final class RealisticGrowth extends JavaPlugin implements Listener {
 
         put(GroundCheck.checkFlowerGround(), Material.DANDELION, Material.POPPY, Material.BLUE_ORCHID, Material.ALLIUM, Material.RED_TULIP, Material.ORANGE_TULIP, Material.WHITE_TULIP, Material.PINK_TULIP, Material.OXEYE_DAISY, Material.CORNFLOWER, Material.LILY_OF_THE_VALLEY, Material.WITHER_ROSE);
 
-        put(GroundCheck.checkFlowerGround(), Material.SUNFLOWER, Material.LILAC, Material.ROSE_BUSH, Material.PEONY);
+        put(GroundCheck.checkLargeFlowerGround(), Material.SUNFLOWER, Material.LILAC, Material.ROSE_BUSH, Material.PEONY);
 
-        put(GroundCheck.checkFlowerGround(), Material.FERN, Material.LARGE_FERN, Material.SHORT_GRASS, Material.TALL_GRASS);
+        put(GroundCheck.checkFlowerGround(), Material.FERN, Material.SHORT_GRASS);
+
+        put(GroundCheck.checkLargeFlowerGround(), Material.LARGE_FERN, Material.TALL_GRASS);
 
         put(GroundCheck.checkMushroomGround(), Material.RED_MUSHROOM, Material.BROWN_MUSHROOM);
 
@@ -74,7 +82,23 @@ public final class RealisticGrowth extends JavaPlugin implements Listener {
         replaces.put(Material.BAMBOO, Material.BAMBOO_SAPLING);
         replaces.put(Material.SWEET_BERRIES, Material.SWEET_BERRY_BUSH);
 
+        registerLarge(Material.TALL_GRASS);
+        registerLarge(Material.LARGE_FERN);
+
+        registerLarge(Material.SUNFLOWER);
+        registerLarge(Material.LILAC);
+        registerLarge(Material.ROSE_BUSH);
+        registerLarge(Material.PEONY);
+
+
         getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    private void registerLarge(Material type) {
+        placeHandler.put(type, l -> {
+            l.getBlock().setType(type);
+            l.clone().add(0, 1, 0).getBlock().setBlockData(Bukkit.createBlockData(type, UPPER));
+        });
     }
 
     private void put(GroundCheck check, Material... types) {
@@ -94,6 +118,7 @@ public final class RealisticGrowth extends JavaPlugin implements Listener {
     public void onDisable() {
         saplings.clear();
         replaces.clear();
+        placeHandler.clear();
     }
 
     private boolean random() {
@@ -102,16 +127,38 @@ public final class RealisticGrowth extends JavaPlugin implements Listener {
         return random.nextDouble() < replant_chance;
     }
 
+    private void place(Location location, Material material) {
+        Consumer<Location> handler = placeHandler.get(material);
+        if (handler==null) location.getBlock().setType(material);
+        else handler.accept(location);
+    }
+
     @EventHandler
     public void onItemDespawn(ItemDespawnEvent event) {
-        Material mat = event.getEntity().getItemStack().getType();
-        GroundCheck check;
-        if ((check = saplings.get(mat))!=null) {
-            if (check.check(event.getLocation()) && random()) {
-                Location location = event.getLocation();
-                mat = replaces.getOrDefault(mat, mat);
+        ItemStack stack = event.getEntity().getItemStack();
+        Material mat = stack.getType();
+        Location location = event.getLocation();
+        SaplingData data;
+        if ((data = saplings.get(mat))!=null) {
+            mat = replaces.getOrDefault(mat, mat);
+            GroundCheck check;
+            if (stack.getAmount() >= 4 && data.largeTree() && (check = data.largeCheck()).check(location) && random()) {
+                GroundCheck.LargeField field = check.getField();
+                Location loc1 = field.getLocation_1();
+                Location loc2 = field.getLocation_2();
+                Location loc3 = field.getLocation_3();
+                Location loc4 = field.getLocation_4();
+
+                getLogger().finest(String.format("replant large tree %s at %s { 1 = { x=%s, y=%s, z=%s }, 2 = { x=%s, y=%s, z=%s }, 3 = { x=%s, y=%s, z=%s }, 4 = { x=%s, y=%s, z=%s } }",
+                        mat.getKey().getKey(), location.getWorld(), loc1.getX(), loc1.getY(), loc1.getZ(), loc2.getX(), loc2.getY(), loc2.getZ(),
+                        loc3.getX(), loc3.getY(), loc3.getZ(), loc4.getX(), loc4.getY(), loc4.getZ()));
+
+                for (Location l : field.getLocations()) {
+                    place(l, mat);
+                }
+            } else if (data.normalCheck().check(location) && random()) {
                 getLogger().finest(String.format("replant %s at %s {x=%s, y=%s, z=%s}", mat.getKey().getKey(), location.getWorld(), location.getX(), location.getX(), location.getZ()));
-                location.getBlock().setType(mat);
+                place(location, mat);
             }
         }
     }
