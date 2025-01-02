3e2615f847861120847dec62d10d4a32b878c56f
cyklon73 <cyklon698@gmail.com>
2025-01-02 16:26:29 +0100

    implemented Permission manager

---
+++ src/main/java/de/cyklon/realisticgrowth/Permission.java
@@ -0,0 +1,15 @@
+package de.cyklon.realisticgrowth;
+
+public final class Permission extends org.bukkit.permissions.Permission {
+
+	public static final Permission UPDATE = perm("update");
+
+	private Permission(String name) {
+		super(name);
+	}
+
+	private static Permission perm(String name) {
+		return new Permission("rg." + name);
+	}
+
+}
