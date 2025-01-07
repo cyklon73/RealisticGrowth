371db79108728c31c3b8d983eec8f7329ae81f49
cyklon73 <cyklon698@gmail.com>
2025-01-07 21:31:41 +0100

    prevent updater blocks main thread

--- src/main/java/de/cyklon/realisticgrowth/command/MainCommand.java
+++ src/main/java/de/cyklon/realisticgrowth/command/MainCommand.java
@@ -1,17 +1,17 @@
 package de.cyklon.realisticgrowth.command;
 
 import de.cyklon.realisticgrowth.RealisticGrowth;
+import de.cyklon.realisticgrowth.util.ColorUtil;
 import de.cyklon.realisticgrowth.spigotmc.Updater;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.hover.content.Text;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
+import org.bukkit.command.*;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,39 +33,81 @@ public class MainCommand implements CommandExecutor, TabCompleter {
 		if (args.length > 0 && args[0].equalsIgnoreCase("update")) {
 			if (!updater.hasChecked()) updater.check(true);
 			if (updater.shouldUpdate()) {
-				if (updater.download()) sender.sendMessage(PREFIX + ChatColor.GREEN + "Update Successfully");
-				else {
-					if (plugin.isCompatibilityMode()) {
-						sender.sendMessage("""
+				sender.sendMessage("%s %sUpdating%s"
+						.formatted(PREFIX, ChatColor.GREEN, ColorUtil.legacyGradient("...", new Color(0x55FF7D), new Color(0x3CEEFF))));
+				Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+					boolean downloaded = updater.download();
+					Bukkit.getScheduler().runTask(plugin, () -> {
+						if (downloaded) {
+							sender.sendMessage(PREFIX.getLegacy() + ChatColor.GREEN + " Update Successfully");
+
+							if (plugin.isCompatibilityMode() || sender instanceof ConsoleCommandSender) {
+								updater.sendMessage(sender, """
+								%sTo apply the changes reload the server with %s/reload %sconfirm
+								%sIf the update is not active after reloading, please try to update manually.
+								""".formatted(ChatColor.GREEN, ChatColor.AQUA, ChatColor.YELLOW, ChatColor.GREEN));
+							} else {
+								sender.spigot().sendMessage(new ComponentBuilder()
+										.append(PREFIX.getComponents())
+										.append(" To apply the changes ")
+										.color(GREEN)
+
+										.append("reload")
+										.reset()
+										.color(YELLOW)
+										.bold(true)
+										.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(ColorUtil.gradient("Reload now", YELLOW, GREEN))))
+										.event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/reload confirm"))
+
+										.append(" the server")
+										.reset()
+										.color(GREEN)
+
+										.append("\n")
+
+										.append(PREFIX.getComponents())
+
+										.append(" If the update is not active after reloading, please try to update manually.")
+										.reset()
+										.color(GREEN)
+
+										.create()
+								);
+							}
+						}
+						else {
+							if (plugin.isCompatibilityMode()) {
+								sender.sendMessage("""
                             %s %s Update failed. Please update manually by downloading the file from %s[SpigotMC] %s
                             %s %s
                             """.formatted(
-								PREFIX, ChatColor.RESET, ChatColor.YELLOW, ChatColor.RESET,
-								PREFIX, updater.getDownloadUrl()
-						));
-					} else {
-						sender.spigot().sendMessage(new ComponentBuilder(PREFIX + " Update failed.\n").color(RED)
-								.append(PREFIX + " Please update manually by downloading the file from ")
-								.append("[SpigotMC]").color(YELLOW)
+										PREFIX, ChatColor.RESET, ChatColor.YELLOW, ChatColor.RESET,
+										PREFIX, updater.getDownloadUrl()
+								));
+							} else {
+								sender.spigot().sendMessage(new ComponentBuilder()
+										.append(PREFIX.getComponents())
+										.append(" Update failed.\n")
+										.color(RED)
+										.append(PREFIX.getComponents())
+										.append(" Please update manually by downloading the file from ")
+										.append("[SpigotMC]").color(YELLOW)
 										.event(new ClickEvent(ClickEvent.Action.OPEN_URL, updater.getDownloadUrl()))
 										.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(new ComponentBuilder("Realistic ").color(GREEN)
 												.append("Growth").color(AQUA)
 												.append(" | ").color(GRAY)
 												.append("SpigotMC").color(GOLD)
 												.create())))
-								.create());
-					}
-					sender.sendMessage(ChatColor.RED + "Update failed. Please update manually");
-				}
+										.create());
+							}
+							sender.sendMessage(ChatColor.RED + "Update failed. Please update manually");
+						}
+					});
+				});
 			}
-			else {
-				sender.sendMessage(ChatColor.RED + "No update available");
-				return false;
-			}
+			else sender.sendMessage(ChatColor.RED + "No update available");
 			return true;
 		}
-
-		sender.sendMessage(ChatColor.RED + "Usage: /realistic-growth update");
 		return false;
 	}
 
