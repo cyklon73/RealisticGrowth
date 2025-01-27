103b2ff3a91c928b158b91b8c1f7e2b41d12ada1
cyklon73 <cyklon698@gmail.com>
2025-01-27 01:27:38 +0100

    created new Modrinth Updater

---
+++ src/main/java/de/cyklon/realisticgrowth/modrinth/Updater.java
@@ -0,0 +1,13 @@
+package de.cyklon.realisticgrowth.modrinth;
+
+public class Updater {
+
+	private static final String RESOURCE_ID = "realistic-growth";
+	private static final String API_URL = "https://api.modrinth.com/v2/";
+	private static final String RESOURCE_URL = "https://modrinth.com/plugin/realistic-growth";
+
+	private void check() {
+
+	}
+
+}
