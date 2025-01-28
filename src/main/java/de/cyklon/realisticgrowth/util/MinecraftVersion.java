f861540e6923afdcea969a29460ddf78dc21d096
cyklon73 <cyklon698@gmail.com>
2025-01-28 10:28:17 +0100

    improved Version comparing

--- src/main/java/de/cyklon/realisticgrowth/util/MinecraftVersion.java
+++ src/main/java/de/cyklon/realisticgrowth/util/MinecraftVersion.java
@@ -25,29 +25,18 @@ public class MinecraftVersion {
 		return smallVersion;
 	}
 
-	public int getVersion() {
-		return baseVersion*100 + bigVersion*10 + smallVersion;
-	}
-
 	/**
-	 * @return true if the given version ist equal or higher
+	 * @return true if the current version ist equal or higher than the given
 	 */
 	public boolean checkVersion(int baseVersion, int bigVersion, int smallVersion) {
-		return checkVersion(baseVersion*100 + bigVersion*10 + smallVersion);
+		return compareVersions(formatted(), "%s.%s.%s".formatted(baseVersion, bigVersion, smallVersion)) >= 0;
 	}
 
 	/**
-	 * @return true if the given version ist equal or higher
+	 * @return true if the current version ist equal or higher than the given
 	 */
 	public boolean checkVersion(MinecraftVersion version) {
-		return checkVersion(version.getVersion());
-	}
-
-	/**
-	 * @return true if the given version ist equal or higher
-	 */
-	public boolean checkVersion(int version) {
-		return getVersion() >= version;
+		return compareVersions(formatted(), version.formatted()) >= 0;
 	}
 
 	public String formatted() {
@@ -66,5 +55,24 @@ public class MinecraftVersion {
 		int bigVersion = Integer.parseInt(versions[1]);
 		int smallVersion = versions.length < 3 ? 0 : Integer.parseInt(versions[2]);
 		return new MinecraftVersion(baseVersion, bigVersion, smallVersion);
+	}
+
+	public static int compareVersions(String version1, String version2) {
+		int comparisonResult = 0;
+
+		String[] version1Splits = version1.split("\\.");
+		String[] version2Splits = version2.split("\\.");
+		int maxLengthOfVersionSplits = Math.max(version1Splits.length, version2Splits.length);
+
+		for (int i = 0; i < maxLengthOfVersionSplits; i++){
+			Integer v1 = i < version1Splits.length ? Integer.parseInt(version1Splits[i]) : 0;
+			Integer v2 = i < version2Splits.length ? Integer.parseInt(version2Splits[i]) : 0;
+			int compare = v1.compareTo(v2);
+			if (compare != 0) {
+				comparisonResult = compare;
+				break;
+			}
+		}
+		return comparisonResult;
 	}
 }
